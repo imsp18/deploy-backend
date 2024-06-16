9faa8c5b37ef64a0fecd4d6934aaa3e6ff4c766c
imsp18 <shivangparmar537@gmail.com>
2024-06-16 23:15:02 +0530

    added dotenv package and deployed a server locally

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res)=>{
+    res.send('shivangparmar')
+})
+app.get('/login', (req, res)=>{
+    res.send('<h1>please login at website </h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
